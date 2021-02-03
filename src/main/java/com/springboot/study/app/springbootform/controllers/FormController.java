0b50d1fd7868ca576be0f6f0814cf5bdbda015a8
Giovanny Jimenez <42052095+giojimen3z@users.noreply.github.com>
2021-02-02 20:53:32 -0500

    feature: adding model flow

--- src/main/java/com/springboot/study/app/springbootform/controllers/FormController.java
+++ src/main/java/com/springboot/study/app/springbootform/controllers/FormController.java
@@ -1,28 +1,41 @@
 package com.springboot.study.app.springbootform.controllers;
 
+import com.springboot.study.app.springbootform.models.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.naming.Binding;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
   @GetMapping("/form")
   public String form(Model model) {
     model.addAttribute("titulo", "Formulario usuario");
+    Usuario usuario = new Usuario();
+    usuario.setNombre("gio");
+    usuario.setApellido("perez");
+    usuario.setId("1234564654k");
+    model.addAttribute("usuario", usuario);
     return "form";
   }
 
   @PostMapping("/form")
   public String procesar(
-      Model model,
-      @RequestParam String username,
-      @RequestParam String password,
-      @RequestParam String email) {
+      @Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
     model.addAttribute("titulo", "Resultado del form");
-    model.addAttribute("username", username);
-    model.addAttribute("password",password);
-    model.addAttribute("email", email);
+    if (result.hasErrors()) {
+      return "form";
+    }
+    model.addAttribute("usuario", usuario);
+    status.setComplete();
     return "resultado";
   }
 }
---
+++ src/main/java/com/springboot/study/app/springbootform/models/domain/Usuario.java
@@ -0,0 +1,27 @@
+package com.springboot.study.app.springbootform.models.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class Usuario {
+
+  private String id;
+
+  @NotEmpty() private String nombre;
+
+  @NotEmpty private String apellido;
+
+  @NotEmpty
+  @Size(min = 3, max = 8)
+  private String username;
+
+  @NotEmpty private String password;
+
+  @Email() @NotEmpty private String email;
+}
